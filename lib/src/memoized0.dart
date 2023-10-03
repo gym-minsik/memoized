b12d79a1c5d60cc8b86864aca6a00724e8b84640
Minsik Kim <103918868+sylfreee@users.noreply.github.com>
2023-10-03 12:19:04 +0900

    Add `isExpired`, `isNotComputedYet`, `isComputed` to `Memoized`

--- lib/src/memoized0.dart
+++ lib/src/memoized0.dart
@@ -1,3 +1,5 @@
+enum MemoizedStatus { notComputed, computed, expired }
+
 /// Represents a memoization wrapper for a computation that returns a value of type [V].
 ///
 /// This class is used to cache and manage the result of a computation so it doesn't
@@ -7,43 +9,43 @@ class Memoized<V> {
   final V Function() _computation;
 
   /// Indicates if the cached value is expired and needs to be recomputed.
-  bool _expired = true;
+  var _status = MemoizedStatus.notComputed;
 
   /// The cached value after computation.
-  V? _memoized;
+  late V _memoized;
 
   Memoized(this._computation);
+
+  /// Check if the memoized value is expired.
+  bool get isExpired => _status == MemoizedStatus.expired;
+
+  /// Check if the memoized value is not computed yet.
+  bool get isNotComputedYet => _status == MemoizedStatus.notComputed;
+
+  /// Check if the memoized value is successfully computed.
+  bool get isComputed => _status == MemoizedStatus.computed;
 
   /// Calls the wrapped computation and returns the cached value.
   ///
   /// If [shouldUpdate] is set to `true` or the value is marked as expired,
   /// it will recompute the value. Otherwise, it will return the cached value.
   V call({bool shouldUpdate = false}) {
-    if (shouldUpdate || _expired) {
+    if (isNotComputedYet || shouldUpdate || isExpired) {
       _memoized = _computation();
-      _expired = false;
+      _status = MemoizedStatus.computed;
     }
     return _memoized!;
   }
 
   /// Marks the cached value as expired, so the next call will trigger a recomputation.
-  void expire() => _expired = true;
+  void expire() => _status == MemoizedStatus.expired;
 
   @override
   String toString() {
-    const notComputedMessage = 'NOT_COMPUTED_YET';
-    final last = _memoized?.toString();
-    String lastComputation;
-    if (last == notComputedMessage) {
-      lastComputation = 'NOT_CALCULATED_YET';
-    } else {
-      lastComputation = notComputedMessage;
-    }
-
     return '$runtimeType {\n'
         '  functionType: $V Function(),\n'
-        '  lastComputation: $lastComputation,\n'
-        '  expired: $_expired\n'
+        '  lastComputation: ${_memoized?.toString()},\n'
+        '  status: $_status\n'
         '}';
   }
 }
