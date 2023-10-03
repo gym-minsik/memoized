b629defa7b5cb3e7085b4c242a5a261391cb9958
Minsik Kim <103918868+sylfreee@users.noreply.github.com>
2023-10-03 15:50:02 +0900

    test: add tests for `Memoized`

---
+++ test/memoized_test.dart
@@ -0,0 +1,46 @@
+import 'package:memoized/memoized.dart';
+import 'package:test/test.dart';
+
+import 'get_elaped_time.dart';
+
+int sum() {
+  return List.generate(30000000, (index) => index)
+      .reduce((value, element) => value + element);
+}
+
+void main() {
+  group('Memoized', () {
+    test('should return the cached value.', () {
+      final memoized = sum.memo;
+      expect(memoized.isNotComputedYet, true);
+      memoized();
+      expect(memoized.isComputed, true);
+
+      expect(
+          getElaspedTime(memoized), lessThan(const Duration(milliseconds: 7)));
+    });
+
+    test('should expire the cached value.', () {
+      final memoized = sum.memo;
+      expect(memoized.isNotComputedYet, true);
+      memoized();
+      expect(memoized.isComputed, true);
+
+      memoized.expire();
+
+      expect(memoized.isExpired, true);
+      expect(getElaspedTime(memoized),
+          greaterThan(const Duration(milliseconds: 7)));
+    });
+
+    test('should be expired manually.', () {
+      final memoized = sum.memo;
+      expect(memoized.isNotComputedYet, true);
+      memoized();
+      expect(memoized.isComputed, true);
+
+      expect(getElaspedTime(() => memoized(shouldUpdate: true)),
+          greaterThan(const Duration(milliseconds: 7)));
+    });
+  });
+}
